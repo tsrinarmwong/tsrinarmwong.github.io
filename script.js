13a08928486b3abc42704b2e6ec1b83442f0fc52
tsrinarmwong <tsrinarmwong@hawk.iit.edu>
2024-07-08 14:23:16 -0500

    Now

--- script.js
+++ script.js
@@ -5,26 +5,25 @@ var projectsSwiper = new Swiper('.projects-swiper', {
     slidesPerView: 1,
     spaceBetween: 10,
     navigation: {
-      nextEl: '.projects-button-next',
-      prevEl: '.projects-button-prev',
+        nextEl: '.projects-button-next',
+        prevEl: '.projects-button-prev',
     },
     pagination: {
-      el: '.projects-pagination',
-      clickable: true,
+        el: '.projects-pagination',
+        clickable: true,
     },
     breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-      },
-      1024: {
-        slidesPerView: 4,
-        spaceBetween: 40,
-      },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
     }
-  });
-  
+});
