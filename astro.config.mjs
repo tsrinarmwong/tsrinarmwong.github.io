bc9373678834ce258725a1c271859d706e7e1df1
Timo Srinarmwong <125150205+tsrinarmwong@users.noreply.github.com>
2026-07-15 19:05:32 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://timobuilds.pro',
+  site: 'https://tsrinarmwong.github.io',
   integrations: [mdx(), sitemap()],
   // The site is a single page now; keep old inbound URLs working.
   redirects: {
